f3e38133e0194195cfe2710e14b867d08c46c1f9
JayMatthews9 <jayrrmatthews@gmail.com>
2025-03-23 21:39:00 +1100

    Added testGetAirplaneId() and testSetAirplaneId() to AirplaneTest.java. Added @DisplayName tags and descriptions to existing test cases.

--- src/test/java/AirplaneTest.java
+++ src/test/java/AirplaneTest.java
@@ -1,5 +1,6 @@
 import com.monash.Airplane;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,22 +20,41 @@ public class AirplaneTest {
     }
 
     @Test
+    @DisplayName("Test whether the airplaneModel field correctly returns the field's expected value.")
     void testGetAirplaneModel() {
         assertEquals("Boeing 747", airplane.getAirplaneModel());
     }
 
     @Test
+    @DisplayName("Test whether the airplaneModel field correctly sets to a new value.")
     void testSetAirplaneModel() {
         airplane.setAirplaneModel("Airbus A350");
         assertEquals("Airbus A350", airplane.getAirplaneModel());
     }
+
     @Test
+    @DisplayName("Test whether the businessSitsNumber field correctly returns the field's expected value.")
     void testGetBusinessSitsNumber() {
         assertEquals(20, airplane.getBusinessSitsNumber());
     }
+
     @Test
+    @DisplayName("Test whether the businessSitsNumber field correctly sets to a new value.")
     void testSetBusinessSitsNumber() {
         airplane.setBusinessSitsNumber(25);
         assertEquals(25, airplane.getBusinessSitsNumber());
+    }
+
+    @Test
+    @DisplayName("Test whether the airplaneID field correctly returns the field's expected value.")
+    void testGetAirplaneId() {
+        assertEquals(22481, airplane.getAirplaneID());
+    }
+
+    @Test
+    @DisplayName("Test whether the airplaneID field correctly sets to a new value.")
+    void testSetAirplaneId() {
+        airplane.setAirplaneID(22113);
+        assertEquals(22113, airplane.getAirplaneID());
     }
 }
